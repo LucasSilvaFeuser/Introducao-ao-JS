3d2a6f0d848159b9d1dd48911cf57058967d2dc1
Lucas Feuser <lucasvinchfeuser@gmail.com>
2022-05-05 13:51:04 -0300

    cond E

--- condicionais.js
+++ condicionais.js
@@ -1,5 +1,5 @@
-var hora = 16;
+var hora = 18;
 
-if ( hora == 12 || hora == 18 ){ // || = condição OU
+if ( hora < 12 && hora < 18 ){ // && = condição E
     console.log("Voce está na hora do rush!");
 } 
