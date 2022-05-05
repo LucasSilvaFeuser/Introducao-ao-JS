ae6923befb14ba8784cee5ab9432b6981d9daf1c
Lucas Feuser <lucasvinchfeuser@gmail.com>
2022-05-05 13:49:21 -0300

    cond OU

--- condicionais.js
+++ condicionais.js
@@ -1,9 +1,5 @@
-var hora = 5;
+var hora = 16;
 
-if (hora < 12 ) {
-   console.log("Bom dia") ;
-}else if (hora < 18) {
-    console.log("Boa tarde");
-}else if ( hora <= 23){
-    console.log("Boa noite");
-}
+if ( hora == 12 || hora == 18 ){ // || = condição OU
+    console.log("Voce está na hora do rush!");
+} 
